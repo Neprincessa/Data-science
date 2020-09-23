f633814a7a5ebc871d88182e0ef1c94ddeb38950
Anastasia Khudoyarova <asya-iriska@yandex.ru>
2020-09-23 15:21:09 +0300

    add sort by rank and abs value for difference

--- лаба1/window_functions/2/C/report_2_2C.sql
+++ лаба1/window_functions/2/C/report_2_2C.sql
@@ -1,11 +1,10 @@
 select CountryRegion, StateProvince, City,
     count([SalesLT].[Customer].CustomerID) as AmountOfClients,
-    (count([SalesLT].[Customer].CustomerID) - lag(count([SalesLT].[Customer].CustomerID)) over (partition by CountryRegion order by count(Customer.CustomerID) desc, City)) as Difference,
+    abs((count([SalesLT].[Customer].CustomerID) - lag(count([SalesLT].[Customer].CustomerID)) over (partition by CountryRegion order by count(Customer.CustomerID) desc, City))) as Difference,
     rank() over (partition by CountryRegion order by count([SalesLT].[Customer].CustomerID) desc, City) as RankAmount
 from [SalesLT].[Customer]
     join [SalesLT].[CustomerAddress] on [SalesLT].[Customer].CustomerID = [SalesLT].[CustomerAddress].CustomerID 
     and [SalesLT].[CustomerAddress].AddressType = 'Main Office'
     join [SalesLT].[Address] on [SalesLT].[Address].AddressID = [SalesLT].[CustomerAddress].AddressID
 group by CountryRegion, StateProvince, City
-order by CountryRegion,City, AmountOfClients desc;
-
+order by CountryRegion, City, RankAmount, AmountOfClients desc;
